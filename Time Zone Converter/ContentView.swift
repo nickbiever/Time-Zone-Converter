88747aed4c3beb410186a66eb21094768ce2e95e
Nick Biever <nickbiever01@gmail.com>
2020-10-02 09:44:56 +0200

    Initial Commit

---
+++ Time Zone Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Time Zone Converter
+//
+//  Created by Nick Biever on 02/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Time Zone Converter/Time_Zone_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_Zone_ConverterApp.swift
+//  Time Zone Converter
+//
+//  Created by Nick Biever on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Time_Zone_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
